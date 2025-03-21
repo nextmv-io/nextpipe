0f2735ef8b0b719d362efff892200c8fa99e97d1
Marius Merschformann <marius.merschformann@gmail.com>
2025-03-21 01:15:58 +0100

    Improving foreach example

--- examples/pipeline-foreach/main.py
+++ examples/pipeline-foreach/main.py
@@ -5,31 +5,34 @@ import os
 import nextmv
 import nextmv.cloud
 
-from nextpipe import AppOption, AppRunConfig, FlowSpec, app, foreach, join, needs, optional, step
+from nextpipe import AppOption, AppRunConfig, FlowSpec, app, foreach, join, needs, step
 
 
 class Flow(FlowSpec):
-    @foreach()
+    @foreach()  # Run the successor step for each item in the result list of this step
     @step
     def prepare(data: dict):
-        """Prepares the data."""
+        """
+        Creates 3 copies of the input and configures them for 3 different app parameters.
+        """
         inputs = [copy.deepcopy(data) for _ in range(3)]
         run_configs = [AppRunConfig(input, [AppOption("param", i)]) for i, input in enumerate(inputs)]
         return run_configs
 
     @app(app_id="echo")
     @needs(predecessors=[prepare])
-    @optional(condition=lambda _: True)
     @step
     def solve():
-        """Runs the model."""
+        """
+        Runs the model.
+        """
         pass
 
     @needs(predecessors=[solve])
     @join()  # Collect the results from the previous 'foreach' step and combine them into a list passed as the arg
     @step
-    def enhance(results: list[dict]):
-        """Enhances the result."""
+    def merge(results: list[dict]):
+        """Merges the results."""
         return results
 
 
@@ -46,7 +49,7 @@ def main():
     flow.run()
 
     # Write out the result
-    print(json.dumps(flow.get_result(flow.enhance)))
+    print(json.dumps(flow.get_result(flow.merge)))
 
 
 if __name__ == "__main__":
