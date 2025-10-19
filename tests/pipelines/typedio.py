c37113be4d45492ab5445f0edd9d407f4d3efc29
Marius Merschformann <marius.merschformann@gmail.com>
2025-10-19 23:39:30 +0200

    New multi-file test case

--- tests/pipelines/typedio.py
+++ tests/pipelines/typedio.py
@@ -1,56 +1,114 @@
-import json
+import os
+import shutil
+import tempfile
 
 import nextmv
+import nextmv.cloud
 
 from nextpipe import FlowSpec, app, needs, step
+
+# TODO: remove debug helper
+os.environ["NEXTMV_API_KEY"] = os.getenv("NEXTMV_API_KEY_PROD")
+
+
+options = nextmv.Options(
+    nextmv.Option("input", str, "inputs/", "Path to input file.", False),
+    nextmv.Option("output", str, "outputs/", "Path to output file.", False),
+)
 
 
 # >>> Workflow definition
 class Flow(FlowSpec):
-    @step
-    def prepare(input: dict):
-        """Prepares the data."""
-        output = nextmv.Output(
-            output_format=nextmv.OutputFormat.MULTI_FILE,
-            solution_files=[nextmv.json_solution_file("echo", data=input)],
-        )
-        # TODO: finish test implementation
-        raise NotImplementedError("Finish the implementation")
-        return output
-
-    @app(app_id="echo")
-    @needs(predecessors=[prepare])
+    @app(app_id="echo-multi")
     @step
     def solve1():
-        """Runs a model."""
+        """Runs a multi-file model."""
         pass
 
-    @app(app_id="echo")
     @needs(predecessors=[solve1])
     @step
-    def solve2():
-        """Runs another model."""
+    def transform(result_path: str):
+        """Transforms the result for the next step."""
+        # Simply copy the files from the given directory to a new temp directory.
+        with tempfile.TemporaryDirectory() as temp_dir:
+            for file_name in os.listdir(result_path):
+                full_file_name = os.path.join(result_path, file_name)
+                if os.path.isfile(full_file_name):
+                    shutil.copy(full_file_name, temp_dir)
+
+            # Add another small file to demonstrate this step passed.
+            with open(os.path.join(temp_dir, "transformed.txt"), "w") as f:
+                f.write("This file was added in the transform step.\n")
+
+    @app(app_id="echo-multi", content_type=nextmv.InputFormat.MULTI_FILE)
+    @needs(predecessors=[transform])
+    @step
+    def solve2(result: nextmv.cloud.RunResult):
+        """Runs another multi-file model."""
         pass
 
     @needs(predecessors=[solve2])
     @step
-    def enhance(result: dict):
-        """Enhances the result."""
-        output = result["solution"]  # Unwrap the solution
-        output["echo"]["data"]["enhanced"] = True
-        return output
+    def prepare_output(result_path: str):
+        """Transforms the result for the next step."""
+        # Simply copy the files from the given directory to the expected output directory.
+        os.makedirs(options.output, exist_ok=True)
+        for file_name in os.listdir(result_path):
+            full_file_name = os.path.join(result_path, file_name)
+            if os.path.isfile(full_file_name):
+                shutil.copy(full_file_name, options.output)
+
+    # # >>>>
+
+    # @step
+    # def prepare(input: dict):
+    #     """Prepares the data."""
+    #     output = nextmv.Output(
+    #         output_format=nextmv.OutputFormat.MULTI_FILE,
+    #         solution_files=[nextmv.json_solution_file("echo", data=input)],
+    #     )
+    #     return output
+
+    # @app(app_id="echo-multi")
+    # @needs(predecessors=[prepare])
+    # @step
+    # def solve1():
+    #     """Runs a model."""
+    #     pass
+
+    # @app(app_id="echo-multi")
+    # @needs(predecessors=[solve1])
+    # @step
+    # def solve2():
+    #     """Runs another model."""
+    #     pass
+
+    # @app(app_id="echo-multi")
+    # @step
+    # def solve3():
+    #     """Runs another model."""
+    #     pass
+
+    # @needs(predecessors=[solve2, solve3])
+    # @step
+    # def enhance(result_solve2: nextmv.cloud.RunResult, result_solve3: nextmv.cloud.RunResult):
+    #     """Enhances the result."""
+    #     output = nextmv.Output(
+    #         output_format=nextmv.OutputFormat.MULTI_FILE,
+    #         solution_files=[
+    #             nextmv.json_solution_file("enhanced_from_solve2", data=result_solve2.data),
+    #             nextmv.json_solution_file("enhanced_from_solve3", data=result_solve3.data),
+    #         ],
+    #     )
+
+    #     return output
 
 
 def main():
-    # Load input data
-    input = nextmv.load()
-
-    # Run workflow
-    flow = Flow("DecisionFlow", input.data)
+    # Run workflow (simply provide the path to the multi-file input)
+    flow = Flow("DecisionFlow", options.input)
     flow.run()
-
-    # Write out the result
-    print(json.dumps(flow.get_result(flow.enhance)))
+    # The last step of the flow already prepares the output, so no need to specify output here.
 
 
 if __name__ == "__main__":
