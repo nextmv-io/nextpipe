ea152c32623c6ecf0ee4565fb950563091d3f4d5
Marius Merschformann <marius.merschformann@gmail.com>
2025-10-23 19:19:48 +0200

    Making echo-multifile more nextmv style

--- tests/apps/echo-multi/main.py
+++ tests/apps/echo-multi/main.py
@@ -1,112 +1,55 @@
-import argparse
 import glob
-import multiprocessing
 import os
-import sys
 import time
+
+import nextmv
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Logs randomly and echoes the input.")
-    parser.add_argument(
-        "-input",
-        default="inputs",
-        help="Path to input directory. Default is 'inputs'.",
+    options = nextmv.Options(
+        nextmv.Option("input", str, "inputs/", "Path to input file.", False),
+        nextmv.Option("output", str, "outputs/", "Path to output file.", False),
+        nextmv.Option("duration", float, 1.0, "Runtime duration (in seconds).", False),
     )
-    parser.add_argument(
-        "-output",
-        default="outputs/solutions",
-        help="Path to output directory. Default is 'outputs'.",
-    )
-    parser.add_argument(
-        "-duration",
-        type=float,
-        default=300.0,
-        help="Duration of the simulation in seconds.",
-    )
-    parser.add_argument(
-        "-threads",
-        default=0,
-        type=int,
-        help="Number of threads to use for the simulation.",
-    )
-    args = parser.parse_args()
-
-    # Set the number of threads to the number of CPUs if not specified.
-    if args.threads == 0:
-        args.threads = os.cpu_count()
 
     # Read and prepare the input data.
-    input_data = read_input(args.input)
+    input_data = read_input(options.input)
 
     # Log information about the input files.
-    log(f"Size of input files (count: {len(input_data)}):")
+    nextmv.log(f"Size of input files (count: {len(input_data)}):")
     for file_path, content in input_data.items():
-        log(f"  {file_path}: {len(content)} bytes")
+        nextmv.log(f"  {file_path}: {len(content)} bytes")
 
-    # Simulate some load.
-    simulate(args.threads, args.duration)
+    # Sleep for the specified duration.
+    nextmv.log(f"Sleeping for {options.duration} seconds...")
+    time.sleep(options.duration)
+    nextmv.log("Woke up from sleep.")
 
     # Write the output.
-    write_output(args.output, input_data)
-
-
-def worker(timeout: float = 1.0):
-    """
-    Simulates some CPU-intensive computation.
-    """
-    end_time = time.time() + timeout
-    while time.time() < end_time:
-        result = 0
-        for i in range(1000000):
-            result += i
-
-
-def simulate(thread_count: int, duration: float):
-    """
-    Simulates some load for the given duration and number of threads.
-    """
-    log(f"Simulating {thread_count} threads for {duration} seconds.")
-
-    # Start the processes.
-    processes = [multiprocessing.Process(target=worker, args=(duration,)) for _ in range(thread_count)]
-    for process in processes:
-        process.start()
-
-    # Wait for the processes to finish.
-    for process in processes:
-        process.join()
-
-    log("Simulating work done.")
-
-
-def log(message: str) -> None:
-    """Logs a message. We need to use stderr since stdout is used for the solution."""
-
-    print(message, file=sys.stderr)
+    write_output(options.output, input_data)
 
 
 def read_input(input_path: str) -> dict[str, bytes]:
-    """Reads the input files."""
+    """Reads the input files to memory."""
     input_files = glob.glob(os.path.join(input_path, "**/*"), recursive=True)
     content = {}
     for file_path in input_files:
         if os.path.isfile(file_path):
             with open(file_path, "rb") as file:
-                log(f"Reading file: {file_path}")
+                nextmv.log(f"Reading file: {file_path}")
                 content[file_path] = file.read()
     return content
 
 
 def write_output(output_path: str, content: dict[str, bytes]) -> None:
-    """Writes the output files."""
+    """Writes the given output files."""
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     for file_path, data in content.items():
         output_file_path = os.path.join(output_path, os.path.basename(file_path))
         with open(output_file_path, "wb") as file:
-            log(f"Writing file: {output_file_path}")
+            nextmv.log(f"Writing file: {output_file_path}")
             file.write(data)
 
 
